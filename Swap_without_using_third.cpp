d6226b309854474906c8334f4025c1c0ce607f38
sohailshaikh786 <84074486+sohailshaikh786@users.noreply.github.com>
2022-10-21 20:06:20 +0530

    Solved problem #64

---
+++ Swap_without_using_third.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+usnig namespace std;
+
+int main(){
+ 
+  int a = 10;
+  int b = 20;
+  a = a+ b; //30
+  b = a -b; //10
+  a = a-b ; //20
+  cout<<"After swapping number ,a = "<<a<<"b =  "<<b;
+  return 0;
+}
